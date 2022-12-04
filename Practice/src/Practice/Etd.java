c3a875d6da4aa27393d88cfaec4f0f5a3f70061d
inseo <wisberry2022@gmail.com>
2022-12-04 17:33:41 +0900

    12/04 upload

---
+++ Practice/src/Practice/Etd.java
@@ -0,0 +1,39 @@
+package Practice;
+
+class Phone {
+    String name;
+    String company;
+    String storage = "30GB";
+    Phone(String name, String company) {
+        this.name = name;
+        this.company = company;
+        System.out.printf("%s의 %s가 출고되었습니다!", this.company, this.name);
+    }
+}
+
+class iPhone extends Phone {
+    iPhone(String name, String company) {
+        super(name, company);
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getStorage() {
+        return this.storage;
+    }
+
+    public void showProduct() {
+        System.out.printf("%s %s \n", this.company, this.name);
+    }
+}
+
+public class Etd {
+    public static void main(String[] args) {
+        iPhone S1 = new iPhone("아이폰", "애플");
+        System.out.println(S1.getStorage());
+
+    }
+}
